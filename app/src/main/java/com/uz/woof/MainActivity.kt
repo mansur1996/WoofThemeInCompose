1a325bf6229341143e4e3a952afc2ce8b2d9cc72
@mirzayevmn96 <mirzayevmn96@gmail.com>
2023-04-26 13:56:52 +0500

    13:56 26/04/2023

--- app/src/main/java/com/uz/woof/MainActivity.kt
+++ app/src/main/java/com/uz/woof/MainActivity.kt
@@ -5,17 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -64,7 +68,9 @@ fun WoofTopAppBar(modifier: Modifier = Modifier) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
-            modifier = Modifier.size(64.dp).padding(8.dp),
+            modifier = Modifier
+                .size(64.dp)
+                .padding(8.dp),
             painter = painterResource(id = R.drawable.ic_woof_logo),
             contentDescription = null
         )
@@ -77,19 +83,79 @@ fun WoofTopAppBar(modifier: Modifier = Modifier) {
 
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = modifier.padding(8.dp),
         elevation = 4.dp
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-                .background(MaterialTheme.colors.surface)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(MaterialTheme.colors.surface)
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(modifier = Modifier.weight(1f))
+                DogItemButton(
+                    expanded = expanded,
+                    onClick = {expanded = !expanded}
+                )
+            }
+            if (expanded){
+                DogHobby(dog.hobbies)
+            }
         }
+    }
+}
+
+@Composable
+fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+): Unit {
+    IconButton(onClick = onClick) {
+        //todo Icons.Filled.ExpandLess and Icons.Filled.ExpandMore in material-icons-extended library
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(id = R.string.expand_button_content_description)
+        )
+    }
+}
+
+@Composable
+fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp,
+        )
+    ) {
+        Text(
+            text = stringResource(id = R.string.about),
+            style = MaterialTheme.typography.h3
+        )
+
+        Text(
+            text = stringResource(id = dogHobby),
+            style = MaterialTheme.typography.body1
+        )
     }
 }
 
--- app/src/main/java/com/uz/woof/ui/theme/Color.kt
+++ app/src/main/java/com/uz/woof/ui/theme/Color.kt
@@ -7,7 +7,7 @@ val Grey50 = Color(0xFFF8F9FA)
 val Grey900 = Color(0xFF202124)
 val Grey700 = Color(0xFF5F6368)
 val Green50 = Color(0xFFE6F4EA)
-val Green100 = Color(0xFFCEEAD6)
+val Green100 = Color(0xFFF1F3F4)
 
 //Dark Theme
 val White = Color(0xFFFFFFFF)
